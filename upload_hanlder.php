471c8aa513a1de4f90c889a7bc228a924aa5d970
haykaleksanian <haykaleksanian.it@gmail.com>
2023-05-26 15:35:03 +0400

    r

--- upload_hanlder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// var_dump($_POST['name']);
-// var_dump($_POST['caption']);
-// var_dump($_FILES);
-$filename = $_FILES['files']['name'][0];
-$tmp_name = $_FILES['files']['tmp_name'][0];
-
-move_uploaded_file($tmp_name, './uploads/' . $filename);
